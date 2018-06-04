9cb9d65db9bd96cd94465e4dbece5fb2cd7b8eff
DaTikh <baptiste.rogeon@gmail.com>
2018-06-04 22:06:29 +0200

    fixed login with username or email

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,19 @@
 class ApplicationController < ActionController::Base
-      before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-      protected
+  protected
 
-        def configure_permitted_parameters
-          devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-          devise_parameter_sanitizer.permit(:edit,
-                  keys: [
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in,         keys: [:login])
+    devise_parameter_sanitizer.permit(:sign_up,         keys: [:username, :email])
+    devise_parameter_sanitizer.permit(:account_update,  keys: [
                     :username,
                     :lastname,
                     :firstname,
+                    :email,
                     :country,
                     :avatar,
                     :birthday
                     ])
-        end
+  end
 end

8d74873d84640cd07349b1cbc495e7005a043100
DaTikh <baptiste.rogeon@gmail.com>
2018-06-04 21:06:56 +0200

    add login with username or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
+  attr_accessor :login
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions.to_hash).where("lower(username) = :value OR lower(email) = :value", value: login.downcase).first
+      else
+        where(conditions.to_hash).first
+      end
+  end
+
   NAME_REGEX = /\A[[:alpha:]\s'"\-_&@!?()\[\]-]*\z/u
   ZIPCODE_REGEX = /\A[0-9]{2,}\z/
   PHONE_REGEX = /\A\d{10}\z/

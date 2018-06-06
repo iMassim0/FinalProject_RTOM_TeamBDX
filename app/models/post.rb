a752e8e54bd428cdc19734801be9cf39a83230ae
DaTikh <baptiste.rogeon@gmail.com>
2018-06-06 22:14:00 +0200

    validations post/comment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
+
+  validates :title,       length: { maximum: 60 },  presence: true
+  validates :description, length: { maximum: 255 }, presence: true
+  validates :content,                               presence: true
 end

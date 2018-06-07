969b78617942110e6f850760634baf67d33bfafa
iMassim0 <i.max@live.fr>
2018-06-07 14:12:49 +0200

    fix all asset-url

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,6 +8,7 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.paths << Rails.root.join("lib", "assets", "images")
+Rails.application.config.assets.paths << Rails.root.join("lib", "assets", "fonts")
 
 Rails.application.config.assets.precompile += %w( dashboard.css )
 # Precompile additional assets.

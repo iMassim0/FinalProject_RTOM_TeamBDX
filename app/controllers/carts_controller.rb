5100037717f8722e8b3b171444a9b52a3affeeed
DaTikh <baptiste.rogeon@gmail.com>
2018-06-08 00:51:57 +0200

    shop + blog Ajax ready

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,28 +2,35 @@ class CartsController < ApplicationController
 
   def show
     @order_items = current_order.order_items
+      respond_to do |f|
+        f.js { }
+      end
   end
 
   def require
     @user = current_user
+    respond_to do |f|
+      f.js { }
+    end
   end
 
   def payment
     @order = current_order
     @user = current_user
-    @infos = params[:user]
+    @user.update(params_user)
+    respond_to do |f|
+      f.js { }
+    end
   end
 
   def pay
     @order = current_order
     new_charge
     if @charge.save
-      @products = current_order.products
-      @products.each {|o| o.availability = 0}
-      @products.each {|o| o.save}
+      current_order.products.each {|o| o.update(availability: 0)}
       session[:order_id] = nil
       flash[:success] = "Paiement réussi"
-      redirect_to shop_path
+      render 'user/profile'
     else
       flash[:error] = "Problème de paiement"
       redirect_to payment_path
@@ -40,5 +47,11 @@ class CartsController < ApplicationController
       :description => "Charge #{current_user.firstname} #{current_user.lastname} - Order #{@order.id}"
     })
   end
+
+  private
+
+    def params_user
+      params.require(:user).permit(:firstname, :lastname, :del_firstname, :del_lastname, :del_address, :del_zipcode, :del_town, :fac_firstname, :fac_lastname, :fac_address, :fac_zipcode, :fac_town, :phonenumber)
+    end
 
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,7 @@
 class ProductsController < ApplicationController
 
+  before_action :set_product, only: [:edit, :update, :destroy]
+
   def index
     @products = Product.all
   end
@@ -18,11 +20,9 @@ class ProductsController < ApplicationController
   end
 
   def edit
-    @product = Product.find(params[:id])
   end
 
   def update
-    @product = Product.find(params[:id])
     if @product.update_attributes(product_params)
       redirect_to products_path
     else
@@ -31,12 +31,15 @@ class ProductsController < ApplicationController
   end
 
   def destroy
-    @product = Product.find(params[:id])
     @product.destroy
     redirect_to products_path
   end
 
   private
+
+    def set_product
+      @product = Product.find(params[:id])
+    end
 
     def product_params
       params.require(:product).permit(:title, :price)

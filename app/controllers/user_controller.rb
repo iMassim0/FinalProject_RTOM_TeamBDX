2a042a20ac2a9bf7e761c85a709defa6eecd9ac1
iMassim0 <i.max@live.fr>
2018-06-09 02:09:59 +0200

    profile finish

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,6 +3,7 @@ class UserController < ApplicationController
   def profile
     if current_user
       @user_orders = Order.where(user_id: session[:user_id])
+      @user = current_user
     else
       redirect_to new_user_session_path
     end

d9ae0b9ec96985caf8cf9cdb1631e28e4a3dba91
iMassim0 <i.max@live.fr>
2018-06-11 10:30:20 +0200

    profile finish with return

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,12 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+
+function openObject(objectName) {
+    var i;
+    var x = document.getElementsByClassName("tabs");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";
+    }
+    document.getElementById(objectName).style.display = "block";
+}
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_08_124329) do
+ActiveRecord::Schema.define(version: 2018_06_11_072748) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -103,6 +103,7 @@ ActiveRecord::Schema.define(version: 2018_06_08_124329) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "bookorder"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true

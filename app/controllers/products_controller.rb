917475fd1a728ad3040eec83b6612e328afd047e
iMassim0 <i.max@live.fr>
2018-06-12 14:38:43 +0200

    for bab

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -65,6 +65,6 @@ class ProductsController < ApplicationController
     end
 
     def product_params
-      params.require(:product).permit(:title, :price, :description, :size, :published)
+      params.require(:product).permit(:title, :price, :description, :size, :image, :published, :quotation)
     end
 end

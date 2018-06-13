7ce09f76bed9340250ae24c688fb3550e93896d4
iMassim0 <i.max@live.fr>
2018-06-13 20:30:45 +0200

    add tools in name of branch OK

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,20 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: [:destroy]
 
+  helper_method :create_comment
+
   def create
+    @comment = Comment.new(comment_params)
+    respond_to do |format|
+      if @comment.save
+        format.js { }
+      else
+        format.js { }
+      end
+    end
+  end
+
+  def create_comment
     @comment = Comment.new(comment_params)
     respond_to do |format|
       if @comment.save

a58fbd0932e837b0bd3aca1bc4d30b7abef762e3
iMassim0 <i.max@live.fr>
2018-06-13 11:54:27 +0200

    fix problem sanitizer permit :country

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_in,         keys: [:login])
-    devise_parameter_sanitizer.permit(:sign_up,         keys: [:username, :email])
+    devise_parameter_sanitizer.permit(:sign_up,         keys: [:username, :email, :country])
     devise_parameter_sanitizer.permit(:account_update,  keys: [
                     :username,
                     :lastname,

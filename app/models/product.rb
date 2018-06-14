8c33d399feae9189d5748b603de597135a63aeff
iMassim0 <i.max@live.fr>
2018-06-14 11:32:54 +0200

    fix problem cloudinary

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   has_many :order_items
-  attr_accessor :image
-
-  mount_uploader :image, ImageUploader
+  attr_accessor :images
+  mount_uploaders :images, ImageUploader
+  serialize :avatars
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -13,15 +13,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   def public_id
     return "my_folder/" + Cloudinary::Utils.random_public_id;
   end
-  # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
-
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
